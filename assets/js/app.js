9555c41364b66d18cd811285f3d40b3031a248fd
443n0511 <443n20070511@gmail.com>
2021-01-07 19:39:57 +0900

    もう一回やるボタン追加、花びらを消すボタンにfadeout機能を追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const getRandom = getRandomIntInclusive(12, 25);
+const getRandom = getRandomIntInclusive(5, 10);
 
 // 花びらをランダムの数表示する関数
 function newAdd() {
@@ -16,31 +16,46 @@ function newAdd() {
     // divをランダムの数用意する為のループ
     for (let i = 0; i < getRandom; i++) {
         const add = document.createElement('li'); // div 用意
-        add.id = 'hanabira' + (i+1); // １から始まる花びらの idを追加
-        add.textContent = "これは追加した" + (i + 1) + "個目のli"
+        add.id = 'hanabira' + (i); // １から始まる花びらの idを追加
+        add.textContent = "これは" + (i + 1) + "枚目のはなびら"
+        add.classList.add('p-list');//花びらのclassもセット
         parent.appendChild(add);//parentの中の末尾にaddを挿入
-        
+
     }
 }
 
 newAdd();
 
 
-let getId = 1;//花びらのidの初期化
-document.getElementById("js-button").onclick = function () {
+//花びらを消すボタン
+
+let getId = 0;//花びらのidの初期化
+document.getElementById("js-button-primary").onclick = function () {
 
     let setId = document.getElementById('hanabira' + getId);//花びらのidをセット
 
-   // ランダムで取得した数と同じ数だけボタンを押せるようにする
-    if (getId <= getRandom) {
-        document.getElementById("js-button").removeAttribute("disabled");
+    // ランダムで取得した数と同じ数だけボタンを押せるようにする
+    if (getId < (getRandom -1)) {
+        document.getElementById("js-button-primary").removeAttribute("disabled");
         getId++;
-        console.log(setId);
+        setId.classList.add('fadeout');
+        setTimeout(function () {
+            setId.style.display = "none";
+        },1000);
     } else {
         // disabled属性を設定
-        document.getElementById("js-button").setAttribute("disabled", true);
+        document.getElementById("js-button-primary").setAttribute("disabled", true);
+        setId.classList.add('fadeout');
+        setTimeout(function () {
+            setId.style.display = "none";
+        },1000);
     }
 };
 
 
+//リロードボタン
+
+document.getElementById("js-button-reload").onclick = function () {
+    location.reload();
+};
 
