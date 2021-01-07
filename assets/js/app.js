9113bc9c2e7f74749aa567f4969155b78cc4c93c
443n0511 <443n20070511@gmail.com>
2021-01-07 15:49:33 +0900

    ボタンを追加し、ランダムで取得した数と同じ数だけボタンを押せるようにする機能の作成

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,27 +2,44 @@
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); 
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const getRandom = getRandomIntInclusive(12,25);
+const getRandom = getRandomIntInclusive(12, 25);
 
 // 花びらをランダムの数表示する関数
-function newAdd (){
-    const parent= document.getElementById("js-parent");
+function newAdd() {
+    const parent = document.getElementById("js-parent");
 
-    console.log(parent);
+    // console.log(parent);
 
     // divをランダムの数用意する為のループ
-    for(let i=0; i<getRandom ; i++){
-        const add = document.createElement('div'); // div 用意
-        add.id = 'hanabira'+i; // 花びらの id
-        add.textContent= "これは追加した"+(i+1)+"個目のdiv"
+    for (let i = 0; i < getRandom; i++) {
+        const add = document.createElement('li'); // div 用意
+        add.id = 'hanabira' + i; // 花びらの id
+        add.textContent = "これは追加した" + (i + 1) + "個目のli"
         parent.appendChild(add);//parentの中の末尾にaddを挿入
     }
 }
 
 newAdd();
+
+
+let getId = 1;//花びらのidの初期化
+document.getElementById("js-button").onclick = function () {
+
+    let setId = document.getElementById('hanabira' + getId);//花びらのidをセット
+
+   // ランダムで取得した数と同じ数だけボタンを押せるようにする
+    if (getId < getRandom) {
+        document.getElementById("js-button").removeAttribute("disabled");
+        getId++;
+        console.log(setId);
+    } else {
+        // disabled属性を設定
+        document.getElementById("js-button").setAttribute("disabled", true);
+    }
+};
 
 
 
